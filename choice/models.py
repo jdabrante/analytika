15caee212e3c1d0c2fd428906700674664c96913
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2023-11-28 15:28:56 +0000

    Eliminado la viste de vote_choice

--- choice/models.py
+++ choice/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+
 from poll.models import Poll
+
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name='choices')
     name = models.CharField(max_length=100)
+
+    def get_percentaje(self):
+        # Esto puede ser la clave
+        total = Poll.objects.all()
 
     def __str__(self):
         return self.name
--- vote/urls.py
+++ vote/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+
 from . import views
+
 app_name = 'vote'
 
-urlpatterns = [
-    path('/<int:poll_id>', views.vote_choice, name='vote_choice')
-]
+urlpatterns = [path('<int:poll_id>/', views.vote_choice, name='vote_choice')]
